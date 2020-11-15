8d98d2467984968df4f66600e7b050fa7a2d03de
Márkus Krisztián <markuskrisztian1@gmail.com>
2020-11-15 16:46:30 +0100

    Function parameter name loading if included

--- parser/src/main/java/com/mktiti/fsearch/parser/asm/AsmFunctionCollector.kt
+++ parser/src/main/java/com/mktiti/fsearch/parser/asm/AsmFunctionCollector.kt
@@ -7,11 +7,14 @@ import com.mktiti.fsearch.core.repo.TypeResolver
 import com.mktiti.fsearch.core.type.CompleteMinInfo
 import com.mktiti.fsearch.core.type.MinimalInfo
 import com.mktiti.fsearch.core.type.TypeTemplate
+import com.mktiti.fsearch.core.util.liftNull
 import com.mktiti.fsearch.parser.intermediate.DefaultFunctionParser
 import com.mktiti.fsearch.parser.intermediate.JavaSignatureFunctionParser
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
+import java.util.*
+import kotlin.collections.ArrayList
 
 object AsmFunctionCollector {
 
@@ -19,6 +22,22 @@ object AsmFunctionCollector {
         val visitor = AsmFunctionCollectorVisitor(dependencyResolver, infoRepo)
         DefaultAsmCollectorView(visitor).load()
         return visitor.methods
+    }
+
+}
+
+private class AsmFunParamNamesVisitor(
+        private val onEnd: (paramNames: List<String>?) -> Unit
+) : MethodVisitor(Opcodes.ASM8) {
+
+    private val paramNames: MutableList<String?> = LinkedList()
+
+    override fun visitParameter(name: String?, access: Int) {
+        paramNames += name
+    }
+
+    override fun visitEnd() {
+        onEnd(paramNames.liftNull())
     }
 
 }
@@ -65,41 +84,39 @@ private class AsmFunctionCollectorVisitor(
     }
 
     override fun visitMethod(access: Int, name: String, descriptor: String, signature: String?, exceptions: Array<out String>?): MethodVisitor? {
-        if (checkFlag and access == checkFlag) {
+        return if (checkFlag and access == checkFlag) {
             val isStatic = ((access and Opcodes.ACC_STATIC) > 0)
-
-            val info = context.info
-
-            if (info.nameParts.any { it.firstOrNull()?.isDigit() != false } && !isStatic) {
-                // Skip anonymous and local types
-                return null
-            }
-
-            try {
-                val toParse = signature ?: descriptor
-                val parsedSignature = if (isStatic) {
-                    funParser.parseStaticFunction(name, null, toParse)
-                } else {
-                    context.let {
-                        when (it) {
-                            is ContextInfo.Direct -> funParser.parseDirectFunction(name, null, toParse, it.thisType)
-                            is ContextInfo.Template -> funParser.parseTemplateFunction(name, null, toParse, it.thisType)
-                        }
-                    }
+            with(context) {
+                if (info.nameParts.any { it.firstOrNull()?.isDigit() != false } && !isStatic) {
+                    // Skip anonymous and local types
+                    return null
                 }
 
-                collectedMethods += FunctionObj(
-                        info = FunctionInfo(name, info.toString()),
-                        signature = parsedSignature
-                )
+                val toParse = signature ?: descriptor
+                AsmFunParamNamesVisitor { paramNames ->
+                    try {
+                        val parsedSignature = if (isStatic) {
+                            funParser.parseStaticFunction(name, paramNames, toParse)
+                        } else {
+                            when (this) {
+                                is ContextInfo.Direct -> funParser.parseDirectFunction(name, paramNames, toParse, thisType)
+                                is ContextInfo.Template -> funParser.parseTemplateFunction(name, paramNames, toParse, thisType)
+                            }
+                        }
 
-            } catch (e: NotImplementedError) {
-                System.err.println("Failed to parse $info $name :: ${signature ?: descriptor}")
-                e.printStackTrace()
+                        collectedMethods += FunctionObj(
+                                info = FunctionInfo(name, info.toString()),
+                                signature = parsedSignature
+                        )
+                    } catch (e: NotImplementedError) {
+                        System.err.println("Failed to parse $info $name :: ${signature ?: descriptor}")
+                        e.printStackTrace()
+                    }
+                }
             }
+        } else {
+            null
         }
-
-        return null
     }
 
 }

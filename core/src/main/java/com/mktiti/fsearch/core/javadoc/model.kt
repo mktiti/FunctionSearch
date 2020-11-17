b2c6438418db829f8c471c4315fbe4a271a04fb2
Márkus Krisztián <markuskrisztian1@gmail.com>
2020-11-17 14:58:32 +0100

    Function doc simple merge

--- core/src/main/java/com/mktiti/fsearch/core/javadoc/model.kt
+++ core/src/main/java/com/mktiti/fsearch/core/javadoc/model.kt
@@ -7,4 +7,22 @@ data class FunctionDoc(
         val paramNames: List<String>? = null,
         val shortInfo: String? = null,
         val longInfo: String? = null
-)
+) {
+
+    companion object {
+        private fun mergeString(current: String?, other: String?): String? = when {
+            current == null -> other
+            current.isNotBlank() -> current
+            other == null -> current
+            else -> other
+        }
+    }
+
+    fun mergeMissing(other: FunctionDoc): FunctionDoc = FunctionDoc(
+            link = link ?: other.link,
+            paramNames = paramNames ?: other.paramNames,
+            shortInfo = mergeString(shortInfo, other.shortInfo),
+            longInfo = mergeString(longInfo, other.longInfo)
+    )
+
+}

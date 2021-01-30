85a68dd763936323b65a7a6012589538d741f2bd
Krisztián Márkus <markuskrisztian1@gmail.com>
2021-01-30 17:53:56 +0100

    Vue frontend in docker

--- frontend-vue/fsearch-frontend/src/router/index.ts
+++ frontend-vue/fsearch-frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Search from '../views/Search.vue'
+import Todo from "@/views/Todo.vue";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,16 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Search',
     component: Search
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Todo
+  },
+  {
+    path: '/demos',
+    name: 'Demos',
+    component: Todo
   }
 ]
 
---
+++ frontend-vue/fsearch-frontend/src/views/Todo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="todo-div">
+    <span>TODO - In development</span>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Todo"
+}
+</script>
+
+<style scoped>
+#todo-div {
+  padding-top: 50px;
+  max-width: 100%;
+  margin: auto;
+  text-align: center;
+}
+
+#todo-div>span {
+  font-size: 30px;
+  font-weight: bold;
+  color: #8bbeff;
+}
+</style>

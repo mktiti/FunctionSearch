8a8ddf6bc4d36ac52dc428289f7a8024fc112e47
Krisztián Márkus <markuskrisztian1@gmail.com>
2021-03-07 18:02:06 +0000

    Let's encrypt fix

--- facade-docker/letsencrypt-init.sh
+++ facade-docker/letsencrypt-init.sh
@@ -9,7 +9,7 @@ domains=(search.mktiti.com www.search.mktiti.com)
 rsa_key_size=4096
 data_path="/data/fsearch/certbot"
 email="markuskrisztian1@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+docker-compose up --force-recreate -d facade
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
@@ -77,4 +77,4 @@ docker-compose run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker-compose exec facade nginx -s reload
